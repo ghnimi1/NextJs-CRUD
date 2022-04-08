a661415a848c2f837fa5dcd8724963f691b47c00
ghnimihassen <ghnimih@gmail.com>
2022-04-08 17:54:50 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   env: {
-    "PORT": "8080",
-    "CLIENT_URL": "http://localhost:3000",
-    "DB_URI": "mongodb+srv://crud:crud@cluster0.maqeb.mongodb.net/crud?retryWrites=true&w=majority",
+
   }
 }
